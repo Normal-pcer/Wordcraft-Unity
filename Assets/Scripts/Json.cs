cf4372153398c185bcfb9c5fa3ac0abfdcbddadc
normal-pcer <tianyafei2009@outlook.com>
2022-06-27 10:46:06 +0800

    ppnn13%, dkstFeb.1st

--- Assets/Scripts/Json.cs
+++ Assets/Scripts/Json.cs
@@ -1,28 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Json
+public class Json<T>
 {
-    public static Dictionary<string, string> decode(string json)
+    public static Dictionary<string, string> decode_one_floor(string json)
     {
         Dictionary<string, string> result;
         Stack<string> st;
         for (int i = 0; i < json.Length; i += 1)
         {
             char c = json[i];
-            switch (c)
+            if (c == '{')
             {
-                case '"':  // string begin/end
-                    if (st.Peek == "string")
-                    {
-                        st.Pop();
-                    }
-                    else
-                    {
-                        st.Push("string");
-                    }
-                    break;
-                case ':':
+                if (st.Count == 0)
+                    st.Push("dict");
+                else if (st.Peek() == "value")
+                { }
+                else
+                    throw new System.Exception("Invalid JSON");
+            }
+            else if (c == '}')
+            {
+                if (st.Peek() == "value" || st.Peek() == "dict")
+                    st.Pop();
+                else
+                    throw new System.Exception("Invalid JSON");
+            }
+            else if (c == ',')
+            {
+                if (st.Peek() == "value")
+                    st.Pop();
+                else
+                    throw new System.Exception("Invalid JSON");
+            }
+            else if (c == ':')
+            {
+                if (st.Peek() == "dict")
+                    st.Push("value");
+                else
+                    throw new System.Exception("Invalid JSON");
+            }
+            else if (c == '"')
+            {
+                if (st.Peek() == "value")
+                    st.Pop();
+                else
+                    throw new System.Exception("Invalid JSON");
             }
         }
     }
---
+++ Assets/Scripts/Tester.cs
@@ -0,0 +1,11 @@
+public class Tester
+{
+    public static void main(string[] args)
+    {
+        List<int, string> list = new List<int, string>();
+        list.add<string>("hi");
+        list.add<int>("there");
+        list.add<string>(0);
+        list.add<int>(1);
+    }
+}
